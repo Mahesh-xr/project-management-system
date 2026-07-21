7468ac80f226f67ca0f6d80b6cc9de866a9935ca
Mahesh-xr <mahesh.572xr@gmail.com>
2026-07-21 23:37:59 +0530

    Refactor CORS configuration to allow multiple origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://ismo-task-tracker.netlify.app",
+    origin: [
+      "http://localhost:5173",
+      "https://ismo-task-tracker.netlify.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
